43eaf27dc6cfd7a3940119e82cd6da4d7bd8c52c
Sebastian Vilhelm Nielsen <seb@frankly.dk>
2018-04-25 11:59:34 +0200

    remove knex install sequelize

--- start.js
+++ start.js
@@ -1,14 +1,5 @@
-const knex = require('knex');
-const knexConfig = require('./knexfile');
-const { Model, knexSnakeCaseMappers } = require('objection');
-
 // Read dotfile
 require('dotenv').config();
-
-// Initialize knex
-const knexInit = knex({ ...knexConfig[process.env.NODE_ENV], ...knexSnakeCaseMappers() });
-// Bind the models to knex
-Model.knex(knexInit);
 
 const port = process.env.PORT || 3000;
 
