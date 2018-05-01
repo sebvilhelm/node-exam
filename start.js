6822ce921fc44c16cc3689d6f7e18a9af4e50597
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-05-01 10:32:33 +0200

    downgrade nodemon

--- start.js
+++ start.js
@@ -1,12 +1,11 @@
+const app = require('./app');
 const models = require('./models');
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 // Read dotfile
 require('dotenv').config();
 
 const port = process.env.PORT || 3000;
-
-const app = require('./app');
-
-app.set('port', port);
 
 const options = {
   force: false, // TRUE: drop existing tables if they exist
@@ -15,8 +14,10 @@ const options = {
 models.sequelize
   .sync(options)
   .then(() => {
-    app.listen(app.get('port'), () => {
-      console.log(`Express is running on port ${port}`);
-    });
+    server.listen(port);
   })
   .catch(err => console.log("couldn't connect to database", err));
+
+server.on('listening', () => {
+  console.log(`Express is running on port ${port}`);
+});
