3c1aa9d2b43bd0eba90935ced4f4945ac14496ad
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-05-13 17:45:56 +0200

    sample users build script

---
+++ data/sample-data.js
@@ -0,0 +1,37 @@
+const faker = require('faker');
+const { sequelize, User } = require('../models');
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+
+if (process.env.NODE_ENV !== 'development') {
+  console.log('WARNING!!');
+  console.log('Never run this script in production');
+  process.exit();
+}
+
+const getRandomPhoneNumber = () => [...Array(8)].map(() => Math.floor(Math.random() * 9)).join('');
+
+const createUserArray = length =>
+  [...Array(length)].map(() => ({
+    name: faker.name.findName(),
+    email: faker.internet.exampleEmail(),
+    password: bcrypt.hashSync(faker.internet.password(), 12),
+    phoneNumber: getRandomPhoneNumber(),
+    photo: faker.image.avatar(),
+  }));
+
+sequelize
+  .sync()
+  .then(async () => {
+    console.log("Connected to database. Let's generate some users");
+    const users = createUserArray(10);
+    console.log('Users generated, adding to the database...');
+    await User.bulkCreate(users);
+    console.log('Sample data added to database!');
+    process.exit();
+  })
+  .catch(err => {
+    console.log('Something went wrong');
+    console.log(err);
+    process.exit();
+  });
