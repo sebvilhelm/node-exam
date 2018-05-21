1b3ba12f0fd2ac0a19308ed84a98f3246b57e10b
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-05-21 21:02:39 +0200

    lock up

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.post(
 );
 router.get('/chat/:id', authController.isLoggedIn, catchErrors(chatController.showChannel));
 
-router.get('/chat', chatController.getGlobalChat);
+router.get('/chat', authController.isLoggedIn, chatController.getGlobalChat);
 
 router.get('/users', authController.isLoggedIn, catchErrors(userController.userList));
 
