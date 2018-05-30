ec6bfbbd3394902da680d849f721798f1d42981f
VilhelmNielsen <seb.vilhelm@gmail.com>
2018-05-30 12:31:13 +0200

    add comment

--- models/Message.js
+++ models/Message.js
@@ -1,8 +1,20 @@
-/* 
-Message model for messages
-Each message container:
-  id (primary)
-  content
-  author (foreign key)
-  chatroom (foreign key)
-*/
+/**
+ * Message model for messages
+ * Each message container:
+ *  id (primary)
+ *  content
+ *  author (foreign key)
+ *  chatroom (foreign key)
+ */
+
+/**
+ * * Select only the n most recent
+ * model.findOne({
+ *  limit: n,
+ *  where: {
+ *    ...
+ *  },
+ *  order: [ [ 'createdAt', 'DESC' ]],
+ * });
+ *
+ */
